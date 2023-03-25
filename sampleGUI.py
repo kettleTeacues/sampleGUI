9223b14cfa56b1bb219b883078ef5eaed306cd0a
kettleTeacues <teacues@piechika.com>
2023-03-25 18:14:35 +0900

    store history

--- sampleGUI.py
+++ sampleGUI.py
@@ -1,20 +1,28 @@
 import PySimpleGUI as sg
 
-sg.theme('DarkAmber')   # Add a touch of color
-# All the stuff inside your window.
+history = 'bin/history'
+
+with open(history) as f:
+    string = f.readlines()[-1]
+
 layout = [
-    [sg.Text('Some text on Row 1')],
-    [sg.Text('Enter something on Row 2'), sg.InputText()],
-    [sg.Button('Ok'), sg.Button('Cancel')]
+    [sg.Text('this is test GUI')],
+    [sg.Text('Enter something on Row 2'), sg.InputText(string)],
+    [sg.Button('ok'), sg.Button('Cancel')]
 ]
 
 # Create the Window
-window = sg.Window('Window Title', layout)
+window = sg.Window('Window Title', layout, keep_on_top=True)
+
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
         break
+    if event == 'ok':
+        with open(history, 'a') as f:
+            f.write('\n' + values[0])
+    print(event)
     print('You entered ', values[0])
 
 window.close()
